a9ef00d8fe01a52554b4ab677699dc9d6d95a7aa
manuel.klocek <manuel.klocek@gmail.com>
2022-11-30 15:20:29 +0100

    save commit

--- UnoGame/Program.cs
+++ UnoGame/Program.cs
@@ -17,6 +17,8 @@ namespace UnoGame
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            var logic = new Logic();
+            logic.setupCards();
         }
     }
 }
